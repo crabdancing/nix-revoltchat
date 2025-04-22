911bff514357fbc7e139ecefec1a2f04253655fd
Alexandria P. <alxpettit@gmail.com>
2025-04-21 21:52:03 -0600

    packaged stupidly, but it works

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Revolt Desktop — but now actually works on Nix.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: {
+        packages.default = pkgs.callPackage ./pkgs/revolt-desktop.nix {
+          inherit (pkgs) lib stdenv fetchFromGitHub yarn electron makeWrapper;
+        };
+      };
+    };
+}
---
+++ pkgs/revolt-desktop.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  yarn,
+  electron,
+  makeWrapper,
+  rsync,
+}:
+stdenv.mkDerivation rec {
+  pname = "revolt-desktop";
+  version = "0.7.4"; # Change this once you know which rev you're building.
+
+  src = fetchFromGitHub {
+    owner = "revoltchat";
+    repo = "desktop";
+    rev = "d668949c3f5838da3be904ef21d9dfa88cf57af0";
+    hash = "sha256-z7yyAznkSlsIleO+VqNyfRmzxP5ftxJVGyW7KmVyP+k=";
+  };
+
+  nativeBuildInputs = [yarn makeWrapper];
+
+  buildPhase = ''
+    mkdir -p $PWD/.yarn/cache
+    ${lib.getExe rsync} -rs ${../.yarn/cache}/ $PWD/.yarn/cache/
+    export HOME=$PWD
+    export YARN_ENABLE_IMMUTABLE_INSTALLS=true
+    export YARN_CACHE_FOLDER=$PWD/.yarn/cache
+    export YARN_ENABLE_SCRIPTS=false
+    yarn --immutable
+    yarn build:bundle
+  '';
+
+  installPhase = ''
+    mkdir -p $out/opt/revolt
+    cp -r . $out/opt/revolt
+
+    mkdir -p $out/bin
+    makeWrapper ${electron}/bin/electron $out/bin/revolt-desktop \
+      --add-flags "$out/opt/revolt"
+
+    mkdir -p $out/share/applications
+    cp revolt-desktop.desktop $out/share/applications/
+    substituteInPlace $out/share/applications/revolt-desktop.desktop \
+      --replace "Exec=./revolt-desktop.sh" "Exec=revolt-desktop"
+
+    mkdir -p $out/share/icons/hicolor/512x512/apps
+    cp build/icons/512x512.png $out/share/icons/hicolor/512x512/apps/revolt-desktop.png
+  '';
+
+  meta = with lib; {
+    description = "Unofficial Revolt desktop client — now with more reproducibility.";
+    homepage = "https://github.com/revoltchat/desktop";
+    license = licenses.agpl3Plus;
+    maintainers = [];
+    platforms = platforms.all;
+  };
+}
